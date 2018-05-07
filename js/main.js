b1f017c49553cb66c218767395acd64e79442d55
Pedro Reis <pedroreisv@gmail.com>
2018-05-06 22:04:14 -0300

    Add download project + open project

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ function $(id) {
   return document.getElementById(id);
 }
 
+function download(content, fileName, contentType) {
+  var a = document.createElement("a");
+  var file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+
 const canvas = new fabric.Canvas("c", { isDrawingMode: true });
 fabric.Object.prototype.transparentCorners = false;
 
 const clearButton = $("clear");
 const drawButton = $("draw");
+const downloadButton = $("download");
+const openButton = $("open");
 
 clearButton.onclick = () => canvas.clear();
 drawButton.onclick = function() {
@@ -14,4 +24,16 @@ drawButton.onclick = function() {
   drawButton.innerHTML = canvas.isDrawingMode
     ? "Cancel drawing mode"
     : "Enter drawing mode";
+};
+downloadButton.onclick = () =>
+  download(JSON.stringify(canvas), "project.json", "text/plain");
+openButton.onchange = e => {
+  // under maintainance
+  const files = e.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () =>
+    canvas.loadFromJSON(JSON.parse(reader.result), () => canvas.renderAll());
+
+  reader.readAsText(files);
 };
