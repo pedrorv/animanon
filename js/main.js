ba8ad79beea36233eed9139e78f896d5d87f9e2d
Pedro Reis <pedroreisv@gmail.com>
2018-05-19 17:32:53 -0300

    Inicial js refactor. Remove old buttons

--- js/main.js
+++ js/main.js
@@ -1,39 +1,69 @@
-function $(id) {
-  return document.getElementById(id);
-}
+const Animanon = (function() {
+  const canvas = new fabric.Canvas("c", {
+    isDrawingMode: false,
+    backgroundColor: "#fff"
+  });
+  canvas.renderAll();
+  fabric.Object.prototype.transparentCorners = false;
 
-function download(content, fileName, contentType) {
-  var a = document.createElement("a");
-  var file = new Blob([content], { type: contentType });
-  a.href = URL.createObjectURL(file);
-  a.download = fileName;
-  a.click();
-}
+  const clearButton = $("#clear");
+  const drawButton = $("#a-draw");
+  const saveButton = $("#a-save");
+  const openButton = $("#a-open");
+  const selectButton = $("#a-select");
+  const openFile = $("#open");
 
-const canvas = new fabric.Canvas("c", { isDrawingMode: true });
-fabric.Object.prototype.transparentCorners = false;
+  const toolButtons = [drawButton, selectButton].forEach(function(button) {
+    button.on("click", function() {
+      changeTool($(this).attr("id"));
+    });
+  });
 
-const clearButton = $("clear");
-const drawButton = $("draw");
-const downloadButton = $("download");
-const openButton = $("open");
+  function changeTool(id) {
+    $(".btn-tool").each(function() {
+      const tool = $(this);
 
-clearButton.onclick = () => canvas.clear();
-drawButton.onclick = function() {
-  canvas.isDrawingMode = !canvas.isDrawingMode;
-  drawButton.innerHTML = canvas.isDrawingMode
-    ? "Cancel drawing mode"
-    : "Enter drawing mode";
-};
-downloadButton.onclick = () =>
-  download(JSON.stringify(canvas), "project.json", "text/plain");
-openButton.onchange = e => {
-  // under maintainance
-  const files = e.target.files[0];
-  const reader = new FileReader();
+      if (id === tool.attr("id")) {
+        tool.addClass("active");
+        return;
+      }
 
-  reader.onload = () =>
-    canvas.loadFromJSON(JSON.parse(reader.result), () => canvas.renderAll());
+      tool.removeClass("active");
+    });
+  }
 
-  reader.readAsText(files);
-};
+  function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+
+  clearButton.onclick = () => {
+    console.log("clear");
+    canvas.clear();
+  };
+
+  drawButton.on("click", function(event) {
+    canvas.isDrawingMode = true;
+  });
+
+  selectButton.on("click", function(event) {
+    canvas.isDrawingMode = false;
+  });
+
+  saveButton.on("click", function(event) {
+    download(JSON.stringify(canvas), "project.json", "text/plain");
+  });
+
+  openFile.on("change", function(e) {
+    const files = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () =>
+      canvas.loadFromJSON(JSON.parse(reader.result), () => canvas.renderAll());
+
+    reader.readAsText(files);
+  });
+})();
